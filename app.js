a56e05367079a5f5eecb7b0a056dfc1e8f400916
nayanendu naskar <nayan19981@gmail.com>
2022-09-06 15:03:23 +0530

    js part done

---
+++ app.js
@@ -0,0 +1,39 @@
+var initialPrice = document.querySelector("#initial-price");
+var stocksQuantity = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var submitBtn = document.querySelector("#submit-btn");
+var outputBox = document.querySelector("#output-box");
+
+submitBtn.addEventListener('click', submitHandler);
+
+function submitHandler() {
+    var ip = Number(initialPrice.value);
+    var qty = Number(stocksQuantity.value);
+    var cp = Number(currentPrice.value);
+
+    calculateProfitAndLoss(ip, qty, cp);
+}
+
+function calculateProfitAndLoss(initial, quantity, current) {
+    if (initial > current) {
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;
+
+        showOutput(`Hey the loss is ${loss} and the percent is ${lossPercentage}%`);
+
+    } else if (current > initial) {
+        var profit = (current - initial) * quantity;
+        var profitPercentage = (profit / initial) * 100;
+
+        showOutput(`Hey the profit is ${profit} and the percent is ${profitPercentage}%`);
+
+    } else {
+showOutput(`No pain no gain and no gain no pain`);
+    }
+
+}
+
+
+function showOutput(message) {
+outputBox.innerHTML = massage;
+}
